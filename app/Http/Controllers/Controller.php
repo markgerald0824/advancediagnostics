f6630d150f0d2f420a2f82e4795f7b290cfa41fe
markgerald0824 <markgerald08.24@gmail.com>
2022-02-25 18:11:33 +0800

    add initial http request;

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Http;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function http( $type = "GET", $url = "", $data = [] ) {
+        $request = Http::withHeaders([
+            'Authorization' => 'Bearer c2b647092cbb6f3ba01f0bc66b42f502'
+        ]);
+
+        if ( $type == "GET" ) {
+            $request = $request->get( $url );
+
+        } else {
+            $request = $request->post( $url, $data );
+        }
+
+        return $request;
+    }
 }
--- app/Http/Controllers/ExternalController.php
+++ app/Http/Controllers/ExternalController.php
@@ -7,10 +7,13 @@ use Illuminate\Http\Request;
 class ExternalController extends Controller
 {
     public function validateId( Request $request ) {
+        $url = "https://sandbox.api.business.govt.nz/services/v4/nzbn/entities/{$request->id}";
+        $validateId = $this->http( "GET", $url );
+
         return response()->json([
             'success' => true,
             'message' => 'You made it here!',
-            'data' => $request->all()
+            'data' => $validateId
         ]);
     }
 }
